3a9f6aeccfe023f4e7c591b454b3b3e57354d191
Mark Likhachev <gogomecourier@gmail.com>
2023-03-22 15:23:22 +0300

    Локальное окружение #4

--- api/config/settings/base.py
+++ api/config/settings/base.py
@@ -22,12 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "secret")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
---
+++ api/config/settings/local.py
@@ -0,0 +1,9 @@
+from .base import *  # noqa
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+CACHE_MIDDLEWARE_ALIAS = "default"
+CACHE_MIDDLEWARE_SECONDS = 5
+CACHE_MIDDLEWARE_KEY_PREFIX = ""
