d2dd222005eb2053e5f2469256eacd4b667d6155
Mark Likhachev <gogomecourier@gmail.com>
2023-04-19 10:57:58 +0300

    feat: Приложение `rss` (#23)
    - Приложение `rss` #21

--- api/config/settings/base.py
+++ api/config/settings/base.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -31,6 +35,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd-party apps
+    "rest_framework",
+    # Local apps
+    "apps.rss.apps.RssConfig",
 ]
 
 MIDDLEWARE = [
