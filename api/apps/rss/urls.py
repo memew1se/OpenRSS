e892dd4d3871725afd84879c3248b83cfb7e3330
Mark Likhachev <gogomecourier@gmail.com>
2023-04-20 14:25:20 +0300

    Эндпоинт конкретного MainFeed #16

--- api/apps/rss/urls.py
+++ api/apps/rss/urls.py
@@ -1,7 +1,12 @@
 from django.urls import re_path
 
-from .views import MainFeedListView
+from .views import MainFeedListView, MainFeedRetrieveView
 
 urlpatterns = (
-    re_path("^main-feed/?$", MainFeedListView.as_view(), name="rss-main-feed-list"),
+    re_path(r"^main-feed/?$", MainFeedListView.as_view(), name="rss-main-feed-list"),
+    re_path(
+        r"^main-feed/(?P<slug>[\w-]+)/?$",
+        MainFeedRetrieveView.as_view(),
+        name="rss-main-feed-retrieve",
+    ),
 )
--- api/apps/rss/views.py
+++ api/apps/rss/views.py
@@ -1,6 +1,10 @@
+import feedparser
 from adrf.views import APIView as AsyncAPIView
+from feedparser import FeedParserDict
 from rest_framework.request import Request
 from rest_framework.response import Response
+
+from config import shared
 
 from .models import MainFeed
 
@@ -13,3 +17,15 @@ class MainFeedListView(AsyncAPIView):
             async for feed in mf
         ]
         return Response(data)
+
+
+class MainFeedRetrieveView(AsyncAPIView):
+    async def get(self, request: Request, **kwargs: dict):
+        slug = kwargs["slug"]
+        feed = await MainFeed.objects.aget(slug=slug)
+        async with shared.AIOHTTP_SESSION.get(feed.url) as resp:
+            text = await resp.text()
+        fd: FeedParserDict = feedparser.parse(text)
+        rss = {"channel": fd.channel, "entries": fd.entries}
+
+        return Response(rss)
