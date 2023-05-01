b1c5160e3632b55ea83d4033ffacb07e8799c5c5
Mark Likhachev <gogomecourier@gmail.com>
2023-05-01 12:52:12 +0300

    Настройка tailwind #32

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,7 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import svgLoader from "vite-svg-loader"
 export default defineNuxtConfig({
+  css: ['~/assets/css/main.css'],
   modules: ['@nuxtjs/tailwindcss'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   vite: {
     plugins: [svgLoader()],
   },
