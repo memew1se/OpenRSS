24f658c83472a605996c96cd3e2e9017003eaa69
Mark Likhachev <gogomecourier@gmail.com>
2023-05-01 17:14:08 +0300

    Header #32

---
+++ web/components/the-header/TheHeader.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheLogo from "~/components/TheLogo.vue"
+</script>
+
+<template>
+  <header class="sticky top-0 w-full h-screen">
+    <div class="bg-primary flex h-32 items-center justify-between p-10">
+        <div></div>
+        <TheLogo />
+    </div>
+  </header>
+</template>
