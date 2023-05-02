0a59dfffd17efa0db6bfb472ebedffe318bb219d
Mark Likhachev <gogomecourier@gmail.com>
2023-05-02 15:20:22 +0300

    Pinia #34

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import svgLoader from "vite-svg-loader"
 export default defineNuxtConfig({
-  css: ['~/assets/css/main.css'],
-  modules: ['@nuxtjs/tailwindcss'],
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxtjs/tailwindcss", '@pinia/nuxt',],
   postcss: {
     plugins: {
       tailwindcss: {},
