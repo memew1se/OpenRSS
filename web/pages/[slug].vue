f9f29d5d3beb4c3a72f2e449ffb2681d64821feb
Mark Likhachev <gogomecourier@gmail.com>
2023-05-03 14:12:48 +0300

    feat: Компонент блока RSS (#46)
    - Компонент блока RSS #37

--- web/pages/[slug].vue
+++ web/pages/[slug].vue
@@ -1,7 +1,21 @@
 <script setup lang="ts">
-
+const rssentries = [
+  {
+    title: "Very long title this is",
+    link: "https://google.com",
+    date: "2002.02.01",
+  },
+  { title: "Short title", link: "https://google.com", date: "2002.02.02" },
+]
 </script>
 
 <template>
-  <h1 class="text-center font-bold text-5xl">{{ $route.params.slug }}</h1>
+  <h1 class="text-center font-bold text-5xl m-5">{{ $route.params.slug }}</h1>
+  <div
+    class="flex flex-col border border-2 border-dashed border-indigo-600 mx-[10rem] my-10 p-5"
+  >
+    <ul>
+      <TheRSSEntry v-for="entry in rssentries" :rssentry="entry" />
+    </ul>
+  </div>
 </template>
