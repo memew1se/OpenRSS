f804b94fb87ab29c5302ce624b338037c01195ee
Mark Likhachev <gogomecourier@gmail.com>
2023-05-04 13:25:52 +0300

    Настройка api #41

--- api/config/settings/local.py
+++ api/config/settings/local.py
@@ -7,3 +7,10 @@ ALLOWED_HOSTS = ["*"]
 CACHE_MIDDLEWARE_ALIAS = "default"
 CACHE_MIDDLEWARE_SECONDS = 5
 CACHE_MIDDLEWARE_KEY_PREFIX = ""
+
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": (
+        # "rest_framework.renderers.BrowsableAPIRenderer",
+        "rest_framework.renderers.JSONRenderer",
+    )
+}
