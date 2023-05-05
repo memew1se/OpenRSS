2e54b0d554a572b9ae0ec0cae71ffee5c7ddfd01
Mark Likhachev <gogomecourier@gmail.com>
2023-05-05 16:02:49 +0300

    feat: Настройка `docker-compose.yml` (#56)
    * Фикс nginx #53
    
    * Настройка статики и медиа #53
    
    * Фикс web #53
    
    * Фикс api #53
    
    * Настройка docker compose #53

--- api/config/settings/base.py
+++ api/config/settings/base.py
@@ -121,7 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "assets/static")
+STATIC_URL = "/api/static/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "assets/media")
+MEDIA_URL = "/api/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
