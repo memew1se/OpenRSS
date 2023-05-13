0c876591d503c2b4ddf1f6749166e658253b80ff
Mark Likhachev <gogomecourier@gmail.com>
2023-05-13 18:31:39 +0300

    feat: Настройка Redis (#60)
    * Библиотека redis #17
    
    * Настройка redis #17

--- api/config/settings/base.py
+++ api/config/settings/base.py
@@ -133,3 +133,10 @@ MEDIA_URL = "/api/media/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 shared.AIOHTTP_SESSION = ClientSession()
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": os.environ.get("REDIS_URL", "redis://redis:6379"),
+    }
+}
